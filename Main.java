64176714718f7791d8d65644ea941436efec45dd
ldsoedt <117372760+ldsoedt@users.noreply.github.com>
2022-12-21 22:32:37 -0600

    added date
    added date

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 import java.text.DecimalFormat;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
 
 public class Main {
     // a project to calculate a price of a rental assuming it is worth $40 for each hour and $1 per minute after that
@@ -12,11 +17,33 @@ public class Main {
         int cHours = hours(iMinutes);
         double cAdditionalMinutes = AdditionalMinutes(iMinutes);
         double cRentalCost = RentalCost(cHours, cAdditionalMinutes);
+        formatDate();
         output(cHours, cAdditionalMinutes, cRentalCost);
 
 
     }
 
+
+    public  static  String formatDate() {
+        String enteredDate;
+        System.out.println("Enter a date (mm/dd/yyyy)");
+        enteredDate = inputDevice.nextLine();
+        String enteredPattern = "MM/dd/yyyy";
+        String newPattern = "MMM dd, yyyy";
+
+
+        SimpleDateFormat sdf = new SimpleDateFormat(enteredPattern);
+        try{
+            Date d = sdf.parse(enteredDate);
+            sdf.applyPattern(newPattern);
+            String newDateString = sdf.format(d);
+            System.out.println("Entered date formatted: " + newDateString);
+        } catch(ParseException e){
+            System.out.println("Invalid Date simulator");
+        }
+
+        return enteredDate;
+    }
     public static int input(){ //gets the minutes the user wants to rent for
        String iData;
        int iMinutes;
@@ -63,12 +90,25 @@ public class Main {
 
 
 
+
+    public  static String getdatefromuser(){
+        String enteredDate;
+        System.out.println("Enter Rental Date (mm/dd/yyyy)");
+        enteredDate = inputDevice.nextLine();
+        return enteredDate;
+    }
+
     private static void output(int iHours, double cAdditionalMinutes, double cRentalCost){
         String oHours, oAdditionalMinutes, oRentalCost;
         oHours = Integer.toString(iHours);
         oAdditionalMinutes = Double.toString(cAdditionalMinutes);
+
         oRentalCost = dollarAmt.format(cRentalCost);
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MMMM dd, yyyy");
+        LocalDate localDate = LocalDate.now();
+        System.out.println(dtf.format(localDate));
         Motto.main();
+
         System.out.printf("%-5s%10s%-17s%10s%-11s\n", "Hours", "", "AdditionalMinutes", "", "Rental Cost");
         System.out.printf("%-5s%10s%-17s%10s%-11s\n", oHours, "", oAdditionalMinutes, "", oRentalCost );
 
